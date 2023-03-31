312f19b2223821c60de23ad3375b8f4c0f38a543
Chinwuba Israel <chiizzy5@gmail.com>
2023-03-31 11:43:07 +0100

    lets go

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main  className="bg-gray-50 w-full overflow-hidden">
+    <main  className={`${inter.className} bg-gray-50 w-full overflow-hidden`}>
       <div className={`bg-gray-50 ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
