b2a868fe3d3aea0b3596fcaf5bc2684428fe70a3
Chinwuba Israel <chiizzy5@gmail.com>
2023-04-14 08:57:08 +0100

    auth

---
+++ lib/auth.ts
@@ -0,0 +1,36 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google"
+import { db } from "./db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const getGoogleCredentials = () =>{
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+
+    if (!clientId || clientId.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_ID')
+    }
+
+    if (!clientSecret || clientSecret.length === 0) {
+        throw new Error('Missing GOOGLE_CLIENT_SECRET')
+    }
+
+  return { clientId, clientSecret };
+};
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
+    session: {
+        strategy: 'jwt'
+    },
+    pages: {
+        signIn: '/login'
+    },
+    providers: [
+        GoogleProvider({
+            clientId: getGoogleCredentials().clientId,
+            clientSecret: getGoogleCredentials().clientSecret,
+        })
+    ]
+}
