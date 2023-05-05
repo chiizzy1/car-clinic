7fc6bf1d9c8b857ff6524b39350e29467e6a02ac
Chinwuba Israel <chiizzy5@gmail.com>
2023-05-05 11:33:44 +0100

    add car repair status track page

--- app/(track)/track/page.tsx
+++ app/(track)/track/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { FC } from "react";
 
-const Track = () => {
+
+const page = () => {
   return (
-    <div>Track Page </div>
-  )
-}
+    <main className="relative flex items-center justify-center">
+      <section className="container max-w-7xl mx-auto">
+        <h1 className="font-bold">TRACKING PAGE</h1>
+      </section>
+    </main>
+  );
+};
 
-export default Track
+export default page;
