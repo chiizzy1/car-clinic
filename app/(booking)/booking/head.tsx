0c67b0ce2729ca779d13ef2fd2e2303cc3aeba0b
Chinwuba Israel <chiizzy5@gmail.com>
2023-05-08 11:28:28 +0100

    booking page add

---
+++ app/(booking)/booking/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Car Clinic | Booking Page</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Clinic for your car" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    )
+}
---
+++ app/(booking)/booking/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const page: FC = ({}) => {
+  return <h1>Book An Appointment</h1>
+}
+
+export default page
