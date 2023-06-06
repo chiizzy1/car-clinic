c20fd38f4c75906975c722f837f9bdf0fbb0e95a
Chinwuba Israel <chiizzy5@gmail.com>
2023-06-07 00:39:20 +0100

    add car types

---
+++ types/api/cars/index.ts
@@ -0,0 +1,33 @@
+import { CarDetails, Customer, Repair } from "@prisma/client";
+import { ZodIssue } from "zod";
+
+export interface CreateNewCarData {
+  error: string | ZodIssue[] | null;
+  CarData: CarDetails | null;
+}
+
+export type allCarData = {
+  id: string;
+  make: string;
+  model: string;
+  year: number;
+  owner: Customer;
+  ownerId: string;
+  repair: Repair[];
+}[];
+
+export interface GetAllCarsData {
+  error: string | ZodIssue[] | null;
+  CarData: allCarData | null;
+}
+[];
+
+export interface DeleteCarData {
+  error: string | ZodIssue[] | null;
+  success: boolean;
+}
+
+export interface UpdateCarData {
+  error: string | ZodIssue[] | null;
+  UpdatedCarData: CarDetails | boolean;
+}
