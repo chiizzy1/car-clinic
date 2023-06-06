1a79d8b94e9c9a636545e23e44032a29008bbbff
Chinwuba Israel <chiizzy5@gmail.com>
2023-06-06 23:38:25 +0100

    add 'get customer' route

---
+++ pages/api/customers/getCustomer.ts
@@ -0,0 +1,54 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { withMethods } from "@/lib/api-middlewares/with-methods";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+import { CreateNewCustomerData } from "@/types/api/customer";
+import { Customer } from "@prisma/client";
+
+const handler = async  (
+  req: NextApiRequest,
+  res: NextApiResponse<CreateNewCustomerData>
+) => {
+    const query = req.query;
+    const { id } = query;
+  try {
+    const user = await getServerSession(req, res, authOptions).then(
+      (data) => data?.user
+    );
+
+    if (user?.role !== "AUTHORIZED") {
+      return res.status(401).json({
+        error: "Unauthorized to perform this action.",
+        customerData: null,
+      });
+    }
+
+    const getCustomerData = await db.customer.findUnique({
+        where: { id: id as string},
+        include: {
+          cars: true,
+          repairs: true,
+        },
+      })
+      
+
+    return res.status(200).json({
+      error: null,
+      customerData: getCustomerData,
+    });
+
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ error: error.issues, customerData: null });
+    }
+
+    return res
+      .status(500)
+      .json({ error: "Internal Server Error", customerData: null });
+  }
+};
+
+export default withMethods(["GET"], handler);
