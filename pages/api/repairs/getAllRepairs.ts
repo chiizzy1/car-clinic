4c7b86624d8a955343b63695a87192e54c7c8071
Chinwuba Israel <chiizzy5@gmail.com>
2023-06-07 15:44:38 +0100

    rename getallcars api

---
+++ pages/api/repairs/getAllRepairs.ts
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { withMethods } from "@/lib/api-middlewares/with-methods";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+import { GetAllRepairsData } from "@/types/api/repairs";
+
+const handler = async  (
+  req: NextApiRequest,
+  res: NextApiResponse<GetAllRepairsData>
+) => {
+  try {
+    const user = await getServerSession(req, res, authOptions).then(
+      (data) => data?.user
+    );
+
+    if (user?.role !== "AUTHORIZED") {
+      return res.status(401).json({
+        error: "Unauthorized to perform this action.",
+        RepairData: null,
+      });
+    }
+
+    const getAllCustomersData = await db.repair.findMany({
+        include: {
+          car: true,
+          owner: true,
+        },
+      })
+      
+
+    return res.status(200).json({
+      error: null,
+      RepairData: getAllCustomersData,
+    });
+
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ error: error.issues, RepairData: null });
+    }
+
+    return res
+      .status(500)
+      .json({ error: "Internal Server Error", RepairData: null });
+  }
+};
+
+export default withMethods(["GET"], handler);
